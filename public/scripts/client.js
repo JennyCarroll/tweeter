cb1f6797513b8b54c89fe9a3aceee0b8f641daac
Jenny Carroll <jennygc@telus.net>
2023-03-01 23:14:33 +0000

    declare loadTweets to fetch tweets from /tweets page and call renderTweets with the return data. update createTweetWithAJAX to create a get request and then take the last element of the arrayOfTweets and pass it to renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,35 +1,8 @@
 /*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ * Client-side JS logic
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
-
+//takes in tweet object and returns $tweet container holding jQuery string html article element populated with object data
 const createTweetElement = function (tweetObj) {
   const $tweet = $(`<article class="tweet">
   <header class="tweets-header">
@@ -47,38 +20,59 @@ const createTweetElement = function (tweetObj) {
   return $tweet;
 };
 
+//runs whem the dom is loaded
 $(document).ready(function () {
   const renderTweets = function (arrOfTweetObjs) {
     for (const obj of arrOfTweetObjs) {
       //leverage createTweetElement function by passing in the tweet object
       const tweet = createTweetElement(obj);
       //use returned jQuery obj by appending each one to the #tweets-container
-      $(".tweets").append(tweet);
+      $(".tweets").prepend(tweet);
     }
   };
-  //first function call when the DOM loads
-  renderTweets(data);
 
-  //handler function submit form data and display new tweet using jQuery so that the page doesn't need to refresh
+  //handler function submit form input data and display new tweet using jQuery so that the page doesn't need to refresh
   const createTweetWithAJAX = function (event) {
     // prevent default behaviour of submit event (data submission and page refresh)
     event.preventDefault();
-    //wrap jquery around the form (event.target) to call .serialize on it,
-    // allowing us to turns a set of form data into a query string
+    //wrap jquery around the form to call .serialize on it, allowing us to turns a set of form data into a query string
     const data = $(event.target).serialize();
     // create AJAX POST request that sends to the server the encoded data string
     $.ajax({
       url: "/tweets",
       data: data,
-      type: "post",
-      //success will run when we get back a succesful http response (200)
+      type: "application/json", //telling ajax what to turn the encoded string into
+      method: "post",
+      //success will run when we get back a succesful http response (200) that means the data succesfully got to the server
       success: (data) => {
-        console.log("post request was successful", data);
-        // renderTweets(data);
+        //extra stuff that could happen after we get a succesful response from the server
+        //clear the textarea by assigning its value to an empty string
+        $("textarea").val("");
+        //new get request to get the last item and pass it to renderTweets which will prepend it to the tweets
+        $.ajax("/tweets", { method: "GET" }).then((arrOfTweets) => {
+          renderTweets(arrOfTweets.slice(-1));
+        });
       },
     });
   };
-  //second function call after dom loads, on submit
-  // add event listenter for submit and call createTweetWithAJAX
+
+  // add event listener for submit and call createTweetWithAJAX
   $("form").on("submit", createTweetWithAJAX);
+
+  //function for fetching a list of tweets from the database (backend)
+  const loadTweets = function () {
+    //use jQuery to make a request to /tweets and receive an array of tweets as JSON
+    $.ajax({
+      url: "/tweets",
+      type: "application/json",
+      method: "get",
+      //success will run when we get back a succesful http response (200)
+      success: (data) => {
+        console.log("get request was successful", data);
+        renderTweets(data);
+      },
+    });
+  };
+  //first function call after the dom loads
+  loadTweets();
 });
