7e80a5b750d6a656dc57eba8be15a86303de3f18
Jenny Carroll <jennygc@telus.net>
2023-03-02 23:28:57 +0000

    clean up code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,22 +2,24 @@
  * Client-side JS logic
  */
 
+//a function to escape some text
 const escape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
+//a function that takes in an array of tweet objects and prepends the new tweet element in the tweets section of index.html
 const renderTweets = function (arrOfTweetObjs) {
   for (const obj of arrOfTweetObjs) {
     //leverage createTweetElement function by passing in the tweet object
     const tweet = createTweetElement(obj);
-    //use returned jQuery obj by appending each one to the #tweets-container
+    //use returned jQuery obj by appending each one to the .tweets section
     $(".tweets").prepend(tweet);
   }
 };
 
-//function for fetching a list of tweets from the database (backend)
+//a function for fetching a list of tweets from the database (backend)
 const loadTweets = function () {
   //use jQuery to make a request to /tweets and receive an array of tweets as JSON
   $.ajax({
@@ -31,14 +33,13 @@ const loadTweets = function () {
   });
 };
 
-//handler function submit form input data and display new tweet using jQuery so that the page doesn't need to refresh
+/* this handler function submits form input data and displays a new tweet using jQuery
+ ** so that the page doesn't need to refresh*/
 const createTweetWithAJAX = function (event) {
   // prevent default behaviour of submit event (data submission and page refresh)
   event.preventDefault();
-
   // disallow form submission if tweet area is empty or exceeds 140 character limit
   const textAreaContent = $("#tweet-text").val();
-
   $(".error").slideUp(400, () => {
     if (textAreaContent === "") {
       $(".error").html("Tweet not present!  Please write tweet.").slideDown();
@@ -49,8 +50,7 @@ const createTweetWithAJAX = function (event) {
       return;
     }
   });
-
-  //wrap jquery around the form to call .serialize on it, allowing us to turns a set of form data into a query string
+  //wrap jquery around the form to call .serialize on it, turning a set of form data into a query string
   const data = $(event.target).serialize();
   // create AJAX POST request that sends to the server the encoded data string
   $.ajax({
@@ -58,8 +58,8 @@ const createTweetWithAJAX = function (event) {
     data: data,
     type: "application/json", //telling ajax what to turn the encoded string into
     method: "post",
-    //success will run when we get back a succesful http response (200) that means the data succesfully got to the server
     success: (data) => {
+      //will run when we get back a succesful http response (200) meaning the data succesfully got to the server
       //extra stuff that could happen after we get a succesful response from the server
       //clear the textarea by assigning its value to an empty string
       $("textarea").val("");
